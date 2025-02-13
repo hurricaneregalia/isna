d545a928a7d79ec81100095ebcd73d07352c7507
hurricaneregalia <88819030+hurricaneregalia@users.noreply.github.com>
2025-02-13 12:11:50 +0700

    navig ok

--- src/app/component/global/imageComponent.js
+++ src/app/component/global/imageComponent.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import React from "react";
 
 export default function ImageComponent({ imageUrl, imageAlt, width, height, priority }) {
-  return <Image src={imageUrl} alt={imageAlt} width={width} height={height} priority={priority} className="rounded-lg w-full" />;
+  return <Image src={imageUrl} alt={imageAlt} width={width} height={height} priority={priority} quality={75} sizes="(max-width: 768px) 100vw, 50vw" className="rounded-lg w-full" />;
 }
--- src/app/component/global/navigation2.js
+++ src/app/component/global/navigation2.js
@@ -1,50 +1,25 @@
 import React from "react";
 import ThemeSwitch from "./themeSwitch";
 import Link from "next/link";
-import logo from "../../../../public/images/siteIdentity/logo.svg";
 import logo2 from "../../../../public/images/siteIdentity/logo2.svg";
 import userPhoto from "../../../../public/images/user/default/userphoto.jpg";
 import Image from "next/image";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { FaXmark } from "react-icons/fa6";
-
 import { Disclosure, DisclosureButton, DisclosurePanel, Transition } from "@headlessui/react";
 import Profile from "./profile";
 import Notif from "./notif";
 
 export default function Navigation2({ siteName }) {
   const navMenu = [
-    {
-      name: "Home",
-      href: "#",
-      key: "home", // Gunakan string unik sebagai key
-    },
-    {
-      name: "Bisnis",
-      href: "#keinginan-pebisnis",
-      key: "bisnis",
-    },
-    {
-      name: "Solusi",
-      href: "#solusi",
-      key: "solusi",
-    },
-    {
-      name: "Layanan",
-      href: "#layanan",
-      key: "layanan",
-    },
-    {
-      name: "Api lokal",
-      href: "/api/lokal",
-      key: "api",
-    },
-    {
-      name: "Login",
-      href: "/login",
-      key: "login",
-    },
+    { name: "Home", href: "#", key: "home" },
+    { name: "Bisnis", href: "#keinginan-pebisnis", key: "bisnis" },
+    { name: "Solusi", href: "#solusi", key: "solusi" },
+    { name: "Layanan", href: "#layanan", key: "layanan" },
+    { name: "Api lokal", href: "/api/lokal", key: "api" },
+    { name: "Login", href: "/login", key: "login" },
   ];
+
   return (
     <Disclosure as="nav" className="bg-base-300">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -60,9 +35,9 @@ export default function Navigation2({ siteName }) {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start" id="desktopNav">
             <div className="hidden sm:ml-6 sm:block w-full">
               <div className="flex flex-row-reverse">
-                <div className="space-x-4 ">
+                <div className="space-x-4">
                   {navMenu.map((item) => (
-                    <Link key={item.name} href={item.href} aria-current={item.current ? "page" : undefined} className="text-base-content">
+                    <Link key={item.key} href={item.href} aria-current={item.current ? "page" : undefined} className="text-base-content">
                       <DisclosureButton as="span">{item.name}</DisclosureButton>
                     </Link>
                   ))}
@@ -74,7 +49,11 @@ export default function Navigation2({ siteName }) {
             <Notif />
             <ThemeSwitch />
             <Profile userPhoto={userPhoto} />
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-base-content sm:hidden">
+            <DisclosureButton
+              className="group relative inline-flex items-center justify-center rounded-md p-2 text-base-content sm:hidden"
+              aria-expanded="false" // Menambahkan aria-expanded untuk tombol menu
+              aria-controls="mobile-menu" // Menambahkan aria-controls yang mengarah ke panel menu
+            >
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <HiMenuAlt3 aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
@@ -91,7 +70,7 @@ export default function Navigation2({ siteName }) {
         leaveFrom="transform translate-y-0 opacity-100"
         leaveTo="transform -translate-y-2 opacity-0"
       >
-        <DisclosurePanel className="sm:hidden">
+        <DisclosurePanel id="mobile-menu" className="sm:hidden">
           <div className="relative">
             <div className="absolute space-y-1 px-2 pb-3 pt-2 bg-base-300 w-full">
               <div className="px-4">
