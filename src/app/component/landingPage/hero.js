7a4e3e09c5ce99fb49d2fe56d9cb491a696deb11
hurricane <hurricaneuser@gmail.com>
2025-04-17 00:17:43 +0700

    change atribute image

--- src/app/component/landingPage/hero.js
+++ src/app/component/landingPage/hero.js
@@ -8,13 +8,8 @@ export default function Hero({ title, description, btnTxt, bg }) {
 
   return (
     <div className="relative min-h-screen rounded-bl-3xl overflow-hidden">
-      {/* ✅ Ganti background dengan <Image /> */}
-      <Image src={bg} alt={title} fill priority={true} className="object-cover z-0" />
-
-      {/* ✅ Overlay */}
+      <Image src={bg} alt={title} fill priority sizes="100vw" className="object-cover z-0" />
       <div className="absolute inset-0 bg-slate-900/80 z-10"></div>
-
-      {/* ✅ Konten */}
       <div className="relative z-20 flex items-center justify-center text-neutral-content text-center min-h-screen">
         <div className="max-w-3xl px-4">
           <h1 className="mb-5 sm:text-5xl text-3xl font-bold">{title}</h1>
