e34e276acdd28ce8becf4ff3d8c51385225f2db7
hurricane <hurricaneuser@gmail.com>
2025-05-23 18:05:01 +0700

    update lagi

--- src/app/myapi/page.js
+++ src/app/myapi/page.js
@@ -1,20 +1,22 @@
+// src/app/myapi/page.js
 import React from "react";
 import CanvasCursor from "../component/canvasCursor/CanvasCursor";
 import Link from "next/link";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+import { prisma } from "../lib/prisma";
 
 export default async function MyApi() {
-  const res = await fetch(`${BASE_URL}/api/apilist`, { cache: "no-store" });
-  const data = await res.json();
-
-  // Akses data.apilists karena struktur JSON-nya seperti itu
-  const items = Array.isArray(data.apilists) ? data.apilists : [];
+  // Query data directly using Prisma
+  let items = [];
+  try {
+    items = await prisma.apiList.findMany(); // Query API list from database
+  } catch (e) {
+    console.error("❌ Error fetching data from database:", e.message);
+  }
 
   return (
     <div>
       <main className="min-h-full h-screen notFoundBg">
-        <div className="w-full h-full grid  place-items-center bg-slate-900/80 px-6 py-24 sm:py-32 lg:px-8 ">
+        <div className="w-full h-full grid place-items-center bg-slate-900/80 px-6 py-24 sm:py-32 lg:px-8">
           <CanvasCursor />
           <div className="flex flex-col items-center justify-center gap-3">
             <div className="flex gap-2">
@@ -23,7 +25,7 @@ export default async function MyApi() {
                   <Link
                     key={item.id}
                     className="list-col-grow capitalize btn shadow-none bg-slate-300 hover:bg-amber-500 border-0 text-slate-500 hover:text-slate-900"
-                    href={`${BASE_URL}/api/${item.name}`}
+                    href={`${process.env.NEXT_PUBLIC_BASE_URL}/api/${item.name}`}
                   >
                     {item.name}
                   </Link>
