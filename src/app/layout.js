4c21645612832d4140dd263ca41e4a4566638ca4
hurricane <hurricaneuser@gmail.com>
2025-05-28 07:34:26 +0700

    add next font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,16 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import { WebVitals } from "./component/global/WebVitals";
+import { Roboto } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="min-h-screen bg-base-200 text-base-content">
+      <body className={`min-h-screen bg-base-200 text-base-content ${roboto.className}`}>
         <WebVitals />
         {children}
       </body>
