4e17d0d392bdc93642dd02b5db405a4f100f0935
hurricaneregalia <88819030+hurricaneregalia@users.noreply.github.com>
2025-07-23 23:21:29 +0700

    update cta hyzaa

--- src/app/bonus/landingpageTemplate/hyzaa/CTAHyzaa.js
+++ src/app/bonus/landingpageTemplate/hyzaa/CTAHyzaa.js
@@ -16,10 +16,19 @@ export default function CtaHyzaa({ targetId, data }) {
           backgroundPosition: "center",
         }}
       >
-        <div className=" bg-slate-900/90 text-white sm:p-32 p-5 py-16">
-          <div className="lg:w-9/12 w-full mx-auto">
-            <HeaderSection aos="fade-up" label={data.label} headline={data.headline} headlineColor="text-lime-300" labelColor="text-white/75" />
-            <div className="space-y-5 text-center items-center sm:w-10/12 w-full mx-auto">
+        <div className=" bg-slate-900/90 text-white sm:py-32 py-10">
+          <div className="w-full mx-auto">
+            <HeaderSection aos="fade-up" label={data.label} headline={data.headline} headlineColor="text-lime-300 lg:w-4/12 sm:w-6/12 sm:px-0 px-5" labelColor="text-white/75" />
+            <div className="relative bg-red-500 mb-12 flex justify-between">
+              <div className="absolute aspect-square w-16 -left-8 bg-base-100 card rotate-45 top-1/2 -translate-y-1/2 ">
+                <div className="custom-radial text-primary/10 bg-[length:1em_1em] absolute inset-0 -rotate-45 " />
+              </div>
+              <div className="absolute aspect-square w-16 -right-8 bg-base-100 card rotate-45 top-1/2 -translate-y-1/2">
+                <div className="custom-radial text-primary/10 bg-[length:1em_1em] absolute inset-0 -rotate-45 " />
+              </div>
+            </div>
+
+            <div className="space-y-5 text-center items-center lg:w-4/12 sm:w-6/12 w-full mx-auto  sm:px-0 px-5">
               <p className="text-lg opacity-75">{data.description}</p>
               <Divider my={8} />
               <Link href={targetId} className="btn btn-lg btn-primary border-success border  ">
--- src/app/bonus/landingpageTemplate/hyzaa/FiturKeunggulan.js
+++ src/app/bonus/landingpageTemplate/hyzaa/FiturKeunggulan.js
@@ -5,19 +5,19 @@ import Bodytext from "./ui/Bodytext";
 import Title from "./ui/Title";
 import Image from "next/image";
 
-export default function FiturKeunggulan({ paddingX, data }) {
+export default function FiturKeunggulan({ data }) {
   return (
     <Wrapper>
       <HeaderSection label={data.label} headline={data.headline} headlineColor="sm:p-0 px-5" />
-      <div className="grid grid-cols-1 md:grid-cols-2 sm:gap-0 gap-5 mt-8 items-center">
-        <div className="relative w-full sm:h-full h-64">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:gap-0 gap-5 mt-8 items-center">
+        <div className="relative w-full sm:h-full h-64 ">
           <Image src={data.imageUrl} alt="Fitur Sepeda" fill sizes="(max-width: 768px) 100vw, 700px" className="object-contain" data-aos="fade-right" />
         </div>
-        <div className="space-y-5 lg:pr-32 sm:pr-16 px-5">
+        <div className="space-y-5">
           {data.item && data.item.length > 0 ? (
             data.item.map((item, idx) => (
               <div key={item.title} className="bg-base-100 card px-5 py-3 shadow-lg shadow-primary/10" data-aos="fade-up">
-                <div className="flex gap-5 items-center">
+                <div className="flex gap-4 items-center">
                   <div className="text-4xl text-primary flex-shrink-0" aria-hidden="true">
                     {item.icon}
                   </div>
