2d86ad9d106d65ccaecd48219c6f060d570c0cae
hurricaneregalia <88819030+hurricaneregalia@users.noreply.github.com>
2025-12-17 21:24:01 +0700

    delete theme

--- src/app/bonus/landingpageTemplate/halvora/components/ui/HalvoraBodyText.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HalvoraBodyText({ text, className = "", ...props }) {
-  return (
-    <p className={`leading-relaxed ${className}`} {...props}>
-      {text}
-    </p>
-  );
-}
